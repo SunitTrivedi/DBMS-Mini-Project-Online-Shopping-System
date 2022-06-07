b9b4b65c8e95df115c2724af5b5f811e60f841f5
SunitTrivedi <80160811+SunitTrivedi@users.noreply.github.com>
2022-06-07 10:51:04 +0530

    Add files via upload

---
+++ newslettter.php
@@ -0,0 +1,20 @@
+
+
+        
+
+        <div id="newsletter" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+					
+						
+							
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
